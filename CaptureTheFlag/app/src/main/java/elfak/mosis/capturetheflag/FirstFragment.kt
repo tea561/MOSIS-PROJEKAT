6bfb6a1296d2f1c9b2ac75ced098d90470e947a2
tea561 <stamenkovicteodora99@gmail.com>
2022-05-31 23:36:55 +0200

    Login i registracija

--- CaptureTheFlag/app/src/main/java/elfak/mosis/capturetheflag/FirstFragment.kt
+++ CaptureTheFlag/app/src/main/java/elfak/mosis/capturetheflag/FirstFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import elfak.mosis.capturetheflag.databinding.FragmentFirstBinding
 
 /**
@@ -15,6 +23,8 @@ import elfak.mosis.capturetheflag.databinding.FragmentFirstBinding
  */
 class FirstFragment : Fragment() {
 
+    private val database = Firebase.database
+    private val dbRef = database.getReferenceFromUrl("https://capturetheflag-56f1c-default-rtdb.firebaseio.com/")
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -33,13 +43,54 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val username: TextInputLayout = requireView().findViewById<TextInputLayout>(R.id.textInputUsername)
-        val password: TextInputLayout = requireView().findViewById<TextInputLayout>(R.id.textInputPassword)
-
 
         binding.createAccButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        val inputUsername: EditText = requireView().findViewById<TextInputEditText>(R.id.username)
+        val inputPassword: EditText = requireView().findViewById<TextInputEditText>(R.id.password)
+        val loginButton: Button = requireView().findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username: String = inputUsername.text.toString()
+            val password: String = inputPassword.text.toString()
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(
+                    this.context,
+                    "Please enter username and password",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                dbRef.child("users").addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (snapshot.hasChild(username)) {
+                            val dbPassword = snapshot.child(username).child("password")
+                                .getValue(String::class.java)
+                            if (dbPassword.equals(password)) {
+                                Toast.makeText(
+                                    view.context,
+                                    "Successfully logged in",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            } else {
+                                Toast.makeText(view.context, "Wrong password", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        } else {
+                            Toast.makeText(view.context, "User not found", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+                })
+            }
+        }
+
     }
 
     override fun onDestroyView() {
--- CaptureTheFlag/app/src/main/java/elfak/mosis/capturetheflag/SecondFragment.kt
+++ CaptureTheFlag/app/src/main/java/elfak/mosis/capturetheflag/SecondFragment.kt
@@ -12,12 +12,20 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import elfak.mosis.capturetheflag.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
+
+    private val database = Firebase.database
+    private val dbRef = database.getReferenceFromUrl("https://capturetheflag-56f1c-default-rtdb.firebaseio.com/")
 
     private var _binding: FragmentSecondBinding? = null
 
@@ -46,6 +54,7 @@ class SecondFragment : Fragment() {
         val inputPassword: EditText = requireView().findViewById<TextInputEditText>(R.id.password)
         val inputFirstName: EditText = requireView().findViewById<TextInputEditText>(R.id.firstname)
         val inputLastName: EditText = requireView().findViewById<TextInputEditText>(R.id.lastname)
+        val inputPhoneNum: EditText = requireView().findViewById<TextInputEditText>(R.id.phoneNum)
 
         val buttonSignup : Button = requireView().findViewById<Button>(R.id.buttonSignup)
         buttonSignup.isEnabled = false
@@ -56,6 +65,7 @@ class SecondFragment : Fragment() {
                         && (inputPassword.text.length > 0)
                         && (inputFirstName.text.length > 0)
                         && (inputLastName.text.length > 0)
+                        && (inputPhoneNum.text.length > 0)
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -72,6 +82,7 @@ class SecondFragment : Fragment() {
                         && (inputPassword.text.length > 0)
                         && (inputFirstName.text.length > 0)
                         && (inputLastName.text.length > 0)
+                        && (inputPhoneNum.text.length > 0)
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -88,6 +99,7 @@ class SecondFragment : Fragment() {
                         && (inputPassword.text.length > 0)
                         && (inputFirstName.text.length > 0)
                         && (inputLastName.text.length > 0)
+                        && (inputPhoneNum.text.length > 0)
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -104,7 +116,27 @@ class SecondFragment : Fragment() {
                         && (inputPassword.text.length > 0)
                         && (inputFirstName.text.length > 0)
                         && (inputLastName.text.length > 0)
+                        && (inputPhoneNum.text.length > 0)
             }
+
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
+
+        inputPhoneNum.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                buttonSignup.isEnabled = (inputUsername.text.length > 0)
+                        && (inputPassword.text.length > 0)
+                        && (inputFirstName.text.length > 0)
+                        && (inputLastName.text.length > 0)
+                        && (inputPhoneNum.text.length > 0)
+            }
+
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
@@ -115,7 +147,35 @@ class SecondFragment : Fragment() {
         })
 
         buttonSignup.setOnClickListener{
-            Toast.makeText(this.context, "Signup clicked.", Toast.LENGTH_SHORT).show()
+            val username: String = inputUsername.text.toString()
+            val password: String = inputPassword.text.toString()
+            val firstName: String = inputFirstName.text.toString()
+            val lastName: String = inputLastName.text.toString()
+            val phoneNum: String = inputPhoneNum.text.toString()
+
+            //Toast.makeText(this.context, "Signup clicked. $username $password $firstName $lastName $phoneNum", Toast.LENGTH_SHORT).show()
+
+            dbRef.child("users").addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.hasChild(phoneNum)){
+                        Toast.makeText(view.context, "Phone is already registered", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        dbRef.child("users").child(phoneNum).child("firstName").setValue(firstName)
+                        dbRef.child("users").child(phoneNum).child("lastName").setValue(lastName)
+                        dbRef.child("users").child(phoneNum).child("username").setValue(username)
+                        dbRef.child("users").child(phoneNum).child("password").setValue(password)
+
+                        Toast.makeText(view.context, "User registered successfully", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
+
+
         }
     }
 
