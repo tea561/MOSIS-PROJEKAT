2234a867c36c0e1ac28c8b7b3412f6bb1fd50ee1
Nevena Tufegdžić <nevenatufegdzic9@gmail.com>
2022-07-11 15:58:42 +0200

    Update za MapObject.

--- CaptureTheFlag/app/src/main/java/elfak/mosis/capturetheflag/data/MapObject.kt
+++ CaptureTheFlag/app/src/main/java/elfak/mosis/capturetheflag/data/MapObject.kt
@@ -10,7 +10,10 @@ data class MapObject(
     val latitude: Double = 0.0,
     val longitude: Double = 0.0,
     val type: String = "",
-    val timestamp: Long = System.currentTimeMillis()) {
+    val timestamp: Long = System.currentTimeMillis(),
+val riddleImgUrl: String,
+val riddleAnswer: String,
+val team: String) {
     @Exclude
     fun toMap(): Map<String, Any?> {
         return mapOf(
@@ -18,7 +21,10 @@ data class MapObject(
             "longitude" to longitude,
             "type" to type,
             "timestamp" to timestamp,
-            "uid" to uid
+            "uid" to uid,
+            "riddleImgUrl" to riddleImgUrl,
+            "riddleAnswer" to riddleAnswer,
+            "team" to team
         )
     }
 }
